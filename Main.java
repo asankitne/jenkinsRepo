0bb386d78583600727e3b148417d3beb656a4f7f
ankit <asankitne@gmail.com>
2023-09-09 14:52:20 +0530

    first commit

---
+++ Main.java
@@ -0,0 +1,9 @@
+import java.util.Date;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("This is java program");
+        System.out.println("out put for the java program");
+        System.out.println(new Date());
+    }
+}
